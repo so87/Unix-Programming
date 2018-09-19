57772fa881e01d326b39fc09d55f8a713b2511ce
so87 <so87@evansville.edu>
2018-09-19 12:49:48 -0500

    finished second project

--- 2project/lslSrc/lsl.cpp
+++ 2project/lslSrc/lsl.cpp
@@ -49,21 +49,68 @@ int main(int argc, char * argv[])
   // make sure it is there
   if (dp != NULL)
   {
+    int totalSize = 0;
     while (ep = readdir (dp))
     {
       // structs that hold our data
       struct stat buf;
-      struct passwd *permID, *permGP;
+      lstat(ep->d_name, &buf);
+      string fileName = ep->d_name;
+      // make sure to exclude . and .. from the command
+      if((fileName != ".") && (fileName != "..")){ 
+      // get size
+      totalSize += (int)buf.st_size;
 
-      // figure out the block size
-      char* completePath = (char*)malloc(snprintf(NULL, 0, "%s/%s", dirname.c_str(), ep->d_name) + 1);  
-      sprintf(completePath, "%s/%s", dirname.c_str(), ep->d_name);
-      lstat(completePath, &buf);
-    
-      // get file user permissions 
-	permID = getpwnam(buf.st_uid);
-	permGP = getpwuid(buf.st_gid);
+        // get dir
+	if(buf.st_mode & S_IFDIR)
+	  permissions[0] = 'd';
+	else
+	  permissions[0] = '-';
+	// get owner perms
+	if(buf.st_mode & S_IRUSR)
+          permissions[1] = 'r';
+	else
+          permissions[1] = '-';
+	if(buf.st_mode & S_IWUSR)
+          permissions[2] = 'w';
+	else
+          permissions[2] = '-';
+	if(buf.st_mode & S_IXUSR)
+          permissions[3] = 'x';
+        else
+          permissions[3] = '-';
+	// get group permissions
+	if(buf.st_mode & S_IRGRP)
+          permissions[4] = 'r';
+        else
+          permissions[4] = '-';
+        if(buf.st_mode & S_IWGRP)
+          permissions[5] = 'w';
+        else
+          permissions[5] = '-';
+        if(buf.st_mode & S_IXGRP)
+          permissions[6] = 'x';
+        else
+          permissions[6] = '-';
 
+	// get other permissionss
+	if(buf.st_mode & S_IROTH)
+          permissions[7] = 'r';
+        else
+          permissions[7] = '-';
+        if(buf.st_mode & S_IWOTH)
+          permissions[8] = 'w';
+        else
+          permissions[8] = '-';
+        if(buf.st_mode & S_IXOTH)
+          permissions[9] = 'x';
+        else
+          permissions[9] = '-';
+
+	// get file user permissions 
+      string permID = getpwuid(buf.st_uid)->pw_name;
+      string permGP = getgrgid(buf.st_gid)->gr_name;
+      
       // date time Month Day hr:min
       char date[256];
       strcpy(date, ctime(&buf.st_mtime));
@@ -71,17 +118,29 @@ int main(int argc, char * argv[])
       date[12] = '\0';
 
       // print out permissions, owner, group, id, size, date, and file name
-      printf("%-4s %-4s %-7zd %-11s %-20s \n", permID->pw_name,permGP->pw_gid, buf.st_size, date, ep->d_name);
-      free(completePath);
+      cout << setw(11) << left << permissions;
+      cout << setw(2) << left << buf.st_nlink;
+      cout << setw(5) << left << permID;
+      cout << setw(5) << left << permGP;
+      cout << setw(8) << left << buf.st_size;
+      cout << setw(11) << left << date << " ";
+      cout << left << setw(15) << fileName << endl;
+      }
     }
     // close it out
+    totalSize = (totalSize)/(1024);
+    cout << "total " << totalSize << endl;
     (void) closedir (dp);
   }
   else
   {
     cout << "error can't open directory" << endl;
     return 1;
-  }
+  }  
+  // sort that output strig
+
+  // print everything out now that it is organized
+
 
   return 0;
 }
