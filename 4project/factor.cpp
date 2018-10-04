e771de9ef329d50524c62c9c7a094295f2dcb914
U-US\1119969 <1119969@TSCND7513YDZ.us.ray.com>
2018-10-04 10:56:56 -0400

    added project base

---
+++ 4project/factor.cpp
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <algorithm>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// function delclarations
+
+
+// structs
+
+
+// defines
+
+int main()
+{
+    return 0;
+}
+
