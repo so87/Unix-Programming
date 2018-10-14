5034700a9a7192e24a8c2c8d76ee13efeacddf18
so87 <so87@evansville.edu>
2018-10-14 17:21:01 -0400

    the child process never seems to finish. maybe problem with cout, maybe problem with user input value, maybe problemw with sending command to maxima. get garbage back

--- 4project/expand.cpp
+++ 4project/expand.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <algorithm>
-
+#include <signal.h>
 #include <iostream>
 #include <string>
 
@@ -19,6 +19,24 @@ using namespace std;
 
 int main()
 {
+   // ensure you receive proper arguments/input
+
+   // create a process to send to maxima
+
+   // create a process to receive from maxima. make sure you are synced up
+
+   // make sure all processes are closed down properly
+
+   // display the answer back to the user in a formated way
+
     return 0;
 }
+
+// prompt user for input 
+
+// open pipe(not popen) to send to maxima
+
+// open pipe to read data from maxima
+
+// format the data for the user
 
--- 4project/factor.cpp
+++ 4project/factor.cpp
@@ -3,22 +3,83 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <algorithm>
-
+#include <signal.h>
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <sys/wait.h>
 
 using namespace std;
 
-// function delclarations
 
+// "wtr_xampl2.cpp" gives examples of forking and piping information
+// dup and dup2 help read/write information to other programs via standard input/output
 
-// structs
-
-
-// defines
-
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+   // ensure you receive proper arguments/input
+   if (argc != 1) {
+      cerr << "error: impropery usage" << endl;
+      cerr << "syntax: " << argv[0] << endl;
+      exit(1);
+   }
+   // Vars to get information from maxima
+   int fd[2], status;
+   pipe(fd);
+   pid_t pid = fork();
+   string userInput = "";
+   char programOutput[300];
+   string totalOutput = "";
+
+   // change file descriptors of child to talk to maxima
+   if (pid == 0){
+	dup2(fd[0], 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(fd[1]);
+	char *args[] = {"maxima", "-q", NULL};
+	execvp("maxima", args);
+   }
+
+   // keep using maxima until user wants to quit
+   while(userInput != "quit"){
+     // get input from user
+     if(pid != 0){
+	cout << "Input an equation to factor: ";
+        cin >> userInput;
+	cout << endl;
+     }
+     // make the child send that input to maxima
+     if((pid == 0) && (userInput != "quit")){
+	//write to pipe with user's input
+	int bytes = userInput.length();
+	write(fd[1], userInput.c_str(), bytes);
+
+	while(1){
+	  // get output from maxima until we see that character
+	  read(fd[0], programOutput, sizeof(programOutput)-1);
+	  totalOutput += programOutput;
+	  // this character is sent when maxima is done
+	  if(strchr(programOutput,'%') != NULL)
+	    break;
+	}
+	exit(3);
+	cout << "wwooo child made it past break" << endl;
+     }
+     // display that back to user
+     if(pid != 0){
+        printf("From read: %s\n", totalOutput);
+     }
+     totalOutput = "";
+   }
+   // make sure all processes are closed down properly
+   string quitMessage = "quit();";
+   if (pid == 0){
+	int bytes = quitMessage.length();
+	write(fd[1], quitMessage.c_str(), bytes);
+   }
+  
+   cout << pid << " finished" << endl;
+   waitpid(pid, &status, 0); 
+   exit(0);
 }
 
