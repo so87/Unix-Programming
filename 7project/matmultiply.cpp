992ef39d91c8cb8446803ca271ba863903a42cfb
so87 <so87@evansville.edu>
2018-11-20 21:19:25 -0500

    finished project 7 but broken

--- 7project/matmultiply.cpp
+++ 7project/matmultiply.cpp
@@ -5,27 +5,42 @@
 
 using namespace std;
 
-void multS(int** a, int** b,int size);                   
-void multM(int** a, int** b,int size);
+void multSingleThread(int** a, int** b,int size);                   
+void multMultipleThread(int** a, int** b,int size);
+
+#define NUM_THREADS 4 // my computer has 4 cores which makes using 4 threads a good balance of performance
+
+struct arg_struct {
+  int** A;
+  int** B;
+  int size;
+  int j;
+  int i;
+  int k;
+  int answer = 0;
+};
 
 int main(int argc, char *argv[]) {                    
     if (argc != 3) {
-	fprintf(stderr, "usage: s|m matrice_size \n");
+	cerr << "usage: <s | m> <size>" << endl;
 	exit(1);
     }
     
     string choice = argv[1];
+    if ((choice != "s") && (choice !="m")){
+	cerr << "error: choose s or m for the first argument" << endl;
+	return 1;
+    }
     int size = atoi(argv[2]);
-    srand(time(NULL));
 
-    // allocate those matrices on the heap
+    // matrix A
     int** a = new int*[size];
     for(int i = 0; i < size; ++i){
       a[i] = new int[size];
       for(int j = 0; j < size; ++j)
         a[i][j] = rand() % 10;
     }
-
+    // matrix B
     int** b = new int*[size];
     for(int i = 0; i < size; ++i){
       b[i] = new int[size];
@@ -33,35 +48,37 @@ int main(int argc, char *argv[]) {
         b[i][j] = rand() % 10;
     }
 
-    // run with a single or multiple thread
     cout << "__________" << endl;
     if(choice == "m"){
-      multM(a, b, size);
+      cout << "Running with multiple threads" << endl;
+      multMultipleThread(a, b, size);
     }
-    else{ 
-      multS(a, b, size);
+    else if (choice == "s"){ 
+      cout << "Running with one thread" << endl;
+      multSingleThread(a, b, size);
     }
     cout << endl << "___________" << endl;
 
 
-    // delete the heap
+    // delete matrix A on heap
     for(int i = 0; i < size; ++i) {
       delete [] a[i];
     }
     delete [] a; 
+    // delete matrix B on heap
     for(int i = 0; i < size; ++i) {
       delete [] b[i];
     }
     delete [] b;
 
-    
+    // close out those threads
+    pthread_exit(NULL);    
     return 0;
 }
 
 
 // ensure to print out first/last element
-void multS(int** A, int** B,int size) {
-    int C[size][size];
+void multSingleThread(int** A, int** B,int size) {
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -70,7 +87,6 @@ void multS(int** A, int** B,int size) {
                 num += A[i][k] * B[k][j];
             }
 
-            C[i][j] = num;
 	    if(i <= 1 && j <= 1)
               std::cout << num << " ";
 	    if(i == 2 && j == 2)
@@ -84,25 +100,72 @@ void multS(int** A, int** B,int size) {
     }
 }
 
+void *multiply(void* arguments){
+  struct arg_struct *args = (arg_struct*) arguments;
+  int k = args->k;
+  int j = args->j;
+  int i = args->i;
+  args->answer += args->A[i][k] * args->B[k][j];
+}
+
+
 // ensure to print out first/last element
-void multM(int** A, int** B, int size) {
-    int C[size][size];
+void multMultipleThread(int** A, int** B, int size) {
+    pthread_t threads[NUM_THREADS];
+    long t;
+    struct arg_struct args;
+    void *thread_result;
+    args.size = size;
+    args.A = A;
+    args.B = B;
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             int num = 0;
-            for (int k = 0; k < size; k++) {
-                num += A[i][k] * B[k][j];
-            }
+	    args.answer = 0;
+	    int k = 0;
+	    args.i = i;
+	    args.j = j;
+	    for (int k = 0; k < size; k++) {
+		args.k = k;
+		int threadstatus[5];
+		pthread_create (&threads[1], NULL, &multiply, &args);
+		if(k+1 < size){
+		  k++;
+		  args.k = k;
+		  pthread_create (&threads[2], NULL, &multiply, &args);
+		  threadstatus[1] = 1;
+		}
+		if(k+2 < size){
+                  k++;
+                  args.k = k;
+                  pthread_create (&threads[3], NULL, &multiply, &args);
+                  threadstatus[2] = 1;
+                }
+		if(k+3 < size){
+                  k++;
+                  args.k = k;
+                  pthread_create (&threads[4], NULL, &multiply, &args);
+                  threadstatus[3] = 1;
+                }
+		pthread_join(threads[1], &thread_result);
+		if(threadstatus[1])	
+		  pthread_join(threads[2], &thread_result);
+		if(threadstatus[2])
+                  pthread_join(threads[3], &thread_result);
+		if(threadstatus[3])
+                  pthread_join(threads[4], &thread_result);
 
-            C[i][j] = num;
-            if(i <= 1 && j <= 1)
+            }
+	    num = args.answer;
+	    if(i <= 1 && j <= 1)
               std::cout << num << " ";
             if(i == 2 && j == 2)
               std::cout << "    ... ";
 
             if(i+1==size && j+1 == size)
               std::cout << endl << "        "<< num << " ";
+
         }
         if(i <= 1)
           std::cout << std::endl;
